35ee2a65e1e6ba2d054c6994884f59bf1efc6401
Jonas <50378849+PowerDayz@users.noreply.github.com>
2022-05-25 19:20:26 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,37 +2,18 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'Power er sej :3'
-version '0.9'
+version '1.0'
 
 shared_scripts {
 	'config.lua',
 }
 
 client_scripts {
-	'client/client.lua',
+	'client.lua',
 }
 
 server_scripts {
-	'server/server.lua',
+	'server.lua',
 }
-
-escrow_ignore {
-	'config.lua',  -- Ignorer Config filen så den ikke bliver krypteret :)
-	'læsmig.md',
-	'billeder/*.png'
-  }
-
--- exports {
---     "Rig1OilAmount",
--- }
-
--- ui_page 'html/ui.html'
-
--- files {
--- 	'html/ui.html',
--- 	'html/css/main.css',
--- 	'html/js/app.js'
--- }
-
 
 lua54 'yes'
